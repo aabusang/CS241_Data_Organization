ce80b5976ce3b6771ac6c82646093b95ca1e2b1b
Adam Abusang <aabusang@b146-41.cs.unm.edu>
2021-04-23 21:36:06 -0600

    closing proj7

---
+++ proj8/list.c
@@ -0,0 +1,4 @@
+/* The file defines all the functions to be used the program */
+
+
+load
---
+++ proj8/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+
+int main(void)
+{
+  /* 07:45:00 Patient: ID = 1, Pain Level (1-10) = 8, Age = 29 years, Time waiting = 00h09m19s */
+  
+}
