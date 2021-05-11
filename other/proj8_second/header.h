f594a00c198870edd4e5ef7c0d18fafe17221901
Adam Abusang <aabusang@Adams-MBP.domain>
2021-05-10 20:43:34 -0600

    switch to test valgrind

--- other/proj8_second/header.h
+++ other/proj8_second/header.h
@@ -5,14 +5,16 @@
 #include <string.h>
 #include <stdlib.h>
 
-typedef struct Time{
+typedef struct Time
+{
   int hours;
   int minutes;
   int sec;
 	
 } Time;
 
-typedef struct Patient{
+typedef struct Patient
+{
   int id;
   int painLevel;
   int age;
@@ -21,69 +23,32 @@ typedef struct Patient{
   struct Patient* next;
 } Patient;
 
-typedef struct Queue{
+typedef struct Queue
+{
   int pCount;
   struct Patient* start;
   struct Patient* end;
 } Queue;
 
 
-void startTime(Time* t,int h,int m,int s);
+void startTime(Time *t, int h, int m, int s);
+void showTime(FILE *out, Time t, int mode);
+void showPatientData(Patient *p);
+void storePatientData(FILE *out, Patient *p);
+void showData(Queue *queue);
+void showData2(Patient *patients[], const int size);
+Queue *initQueue();
+Patient *initPatient(int id, Time time, int age, int painLevel);
+void enqueue(Queue *queue, Patient *patient);
+void updateTime(Time *time);
+void updateWaitingTime(Queue *queue);
+void dischargePatient(Queue *queue,Patient *patient);
+int tooLate(Patient *patient);
+int arriveBefore(Time source, Time target);
+Patient *getNext(Queue *queue);
+int isArrived(Time arrivalTime, Time currTime);
+void processPatients(FILE *out,Queue *source, const int no_patients);
+void clear(Queue *queue);
 
 
-void showTime(FILE* out,Time t,int mode);
-
-
-void showPatientData(Patient* p);
-
-
-void storePatientData(FILE* out,Patient* p);
-
-
-void showData(Queue* queue);
-
-
-void showData2(Patient* patients[],const int size);
-
-
-Queue* initQueue();
-
-
-
-Patient* initPatient(int id,Time time,int age,int painLevel);
-
-
-void enqueue(Queue* queue, Patient* patient);
-
-
-
-void updateTime(Time* time);
-
-
-void updateWaitingTime(Queue* queue);
-
-
-
-void dischargePatient(Queue* queue,Patient* patient);
-
-
-int tooLate(Patient* patient);
-
-
-int arriveBefore(Time source,Time target);
-
-
-Patient* getNext(Queue* queue);
-
-
-
-int isArrived(Time arrivalTime,Time currTime);
-
-
-
-void processPatients(FILE* out,Queue* source,const int no_patients);
-
-
-
-void clear(Queue* queue);
 #endif /*HEADER_H*/
--- other/proj8_second/main.c
+++ other/proj8_second/main.c
@@ -1,104 +1,117 @@
-/************************************************************************
- * Fatima Adawee
- * project 8
- * This project organizes how pateints should be seen according to certain 
- * set of rules
-************************************************************************/
+/****************
+ * By: Adam Abusang
+ * A program to determine how 
+ * patients are attended to at a clinic
+ ***************************************/
+
 #include "header.h"
 
-int main(int argc, char *argv[] )
+int main(int argc, char *argv[])
 {
-  FILE *in,*out;
-  Queue* queue;
-  Patient* patient;
-  int count = 0,temp =0;
-  char line[255];
+    FILE *inFile, *outFile;
+    Queue *queue;
+    Patient *patient;
+    int count = 0,temp =0;
+    char line[255];
  
-  /* if command line arguments aren't passed*/
-  if(argc < 2){
-    in = fopen("arrivals.log","r");
-    out = fopen("output.txt","w");
-  }
-  else{	
-   in = fopen(argv[1], "r");
-   out = fopen(argv[2],"w");
-  }
+    if (argc == 1)
+    {
+	inFile = fopen("arrivals.log", "r");
+	outFile = fopen("mylist.txt", "w");
+    }
+    else if (argc < 2)
+    {
+	inFile = fopen(argv[1],"r");
+	outFile = fopen("mylist.txt","w");
+    }
+    else if (argc == 3)
+    {	
+	inFile = fopen(argv[1], "r");
+	outFile = fopen(argv[2],"w");
+    }
+    else
+    {
+	printf("Too many arguments");
+    }
 
+    if (!inFile) 
+    {
+	printf("Unable to open input file arrivals.log");
+	return 1;
+    }
+  
+    queue = initQueue();
+    while(fgets(line, 255, inFile) != NULL)
+    {
+	Time time;
+	int id = 0, age = 0, i = 0;
+	int painLevel = 0;
+	// i++ to ignore ':' and ' '
+	while(line[i] != ' ')
+	{
+	    id *= 10;
+	    id += line[i] - '0';
+	    i++;
+	}
+	i++; 
+    
+    
+	temp = 0;
+	while(line[i] != ':')
+	{
+	    temp *= 10;
+	    temp += line[i] - '0';
+	    i++;
+	}
+	i++;
+	time.hours = temp;
+	  
+    
+	temp = 0;
+	while(line[i] != ':')
+	{
+	    temp *= 10;
+	    temp += line[i] - '0';
+	    i++;
+	}
+	i++;
+	time.minutes = temp;
+	  
+	temp = 0;
+	while(line[i] != ' ')
+	{
+	    temp *= 10;
+	    temp += line[i] - '0';
+	    i++;
+	}
+	i++;
+	time.sec = temp;
+    
+    
+	while(line[i] != ' ')
+	{
+	    age *= 10;
+	    age += line[i] - '0';
+	    i++;
+	}
+	i++; 
+	  
+	while(line[i] != '\n')
+	{
+	    painLevel *= 10;
+	    painLevel += line[i] - '0';
+	    i++;
+	}
 
-  if(in == NULL)
-  {
-    printf("Error while opening the file.\n");
-	exit(0);
-  }
-  
-  queue = initQueue();
-  while(fgets(line,255,in) != NULL){
-    int id =0;
-    Time time;
-    int age =0;
-    int painLevel = 0;
-    int i = 0;
-	  
-    /* Save ID*/
-    while(line[i] != ' '){
-      id *= 10;
-      id += line[i] - '0';
-      i++;
+	patient = initPatient(id, time, age, painLevel);
+	enqueue(queue, patient);
+	count++;
     }
-    i++; /* ignore the space*/
-    
-    /* save hours*/
-    temp = 0;
-    while(line[i] != ':'){
-      temp *= 10;
-      temp += line[i] - '0';
-      i++;
-    }
-    i++;	/* ignore ':' */
-    time.hours = temp;
-	  
-    /* save minutes*/
-    temp = 0;
-    while(line[i] != ':'){
-      temp *= 10;
-      temp += line[i] - '0';
-      i++;
-    }
-    i++;	/* ignore ':' */
-    time.minutes = temp;
-	  
-    /* save seconds */
-    temp = 0;
-    while(line[i] != ' '){
-      temp *= 10;
-      temp += line[i] - '0';
-      i++;
-    }
-    i++;	/* ignore ' ' */
-    time.sec = temp;
-    
-    /* Save Age */
-    while(line[i] != ' '){
-      age *= 10;
-      age += line[i] - '0';
-      i++;
-    }
-    i++; /* ignore space */
-	  
-    /* Save Pain Level*/
-    while(line[i] != '\n'){
-      painLevel *= 10;
-      painLevel += line[i] - '0';
-      i++;
-    }
-    patient = initPatient(id,time,age,painLevel);
-    enqueue(queue,patient);
-    count++;
-  }
-  processPatients(out,queue,count);
-  clear(queue);
-  
-  fclose(out);
-  fclose(in);
-  return 0;
+    processPatients(outFile,queue,count);
+    clear(queue);
+
+    fclose(inFile);
+    fclose(outFile);
+
+    return 0;
 }
